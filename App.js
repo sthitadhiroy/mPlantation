2da16f8c9839c5a096e8cd4140f4c19c46849e3f
Sthitadhi Roy <sthitadhiroy@Sthitadhis-MacBook-Air.local>
2020-03-12 23:24:58 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import Navigation from './navigation';
+import {Block} from './components';
+
+// import all used images
+const images = [
+  require('./assets/icons/back.png'),
+  require('./assets/icons/plants.png'),
+  require('./assets/icons/seeds.png'),
+  require('./assets/icons/flowers.png'),
+  require('./assets/icons/sprayers.png'),
+  require('./assets/icons/pots.png'),
+  require('./assets/icons/fertilizers.png'),
+  require('./assets/images/plants_1.png'),
+  require('./assets/images/plants_2.png'),
+  require('./assets/images/plants_3.png'),
+  require('./assets/images/explore_1.png'),
+  require('./assets/images/explore_2.png'),
+  require('./assets/images/explore_3.png'),
+  require('./assets/images/explore_4.png'),
+  require('./assets/images/explore_5.png'),
+  require('./assets/images/explore_6.png'),
+  require('./assets/images/illustration_1.png'),
+  require('./assets/images/illustration_2.png'),
+  require('./assets/images/illustration_3.png'),
+  require('./assets/images/avatar.png'),
+];
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  };
+
+  render() {
+    return (
+      <Block white>
+        <Navigation />
+      </Block>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
